327e31c52c25f74f9fd36033fd57d7179a61affc
Logan halleux <logan@MacBook-Pro-van-Logan.local>
2025-04-09 20:37:49 +0200

    Add details page

--- src/app/models/pokemon-details-dto.interface.ts
+++ src/app/models/pokemon-details-dto.interface.ts
@@ -1,3 +1,42 @@
 export interface PokemonDetailsDto {
+  id: string;
   name: string;
+  types: PokemonType[];
+  height: number;
+  weight: number;
+  hp: number;
+  abilities: Ability[];
+  stats: Stats[];
+  sprites: Sprites;
+  base_experience: number;
+}
+
+export interface PokemonType {
+  slot: number;
+  type: INameAndUrl;
+}
+
+export interface Ability {
+  ability: INameAndUrl;
+  is_hidden: boolean;
+}
+
+export interface INameAndUrl {
+  name: string;
+  url: string;
+}
+
+export interface Stats {
+  base_stat: number;
+  stat: { name: string };
+}
+
+export interface Sprites {
+  back_default: string;
+  front_default: string;
+  other: {
+    'official-artwork': {
+      front_default: string;
+    };
+  };
 }
--- src/app/pages/pokemon-details/pokemon-details.component.ts
+++ src/app/pages/pokemon-details/pokemon-details.component.ts
@@ -1,17 +1,35 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { PokemonsService } from '../../services/pokemons.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-pokemon-details',
-  imports: [AsyncPipe],
+  imports: [
+    AsyncPipe,
+    MatCardModule,
+    MatButtonModule,
+    MatAccordion,
+    MatExpansionModule,
+    MatIconModule,
+    MatChipsModule,
+    MatListModule,
+    MatProgressBarModule,
+  ],
   templateUrl: './pokemon-details.component.html',
   styleUrl: './pokemon-details.component.scss',
 })
 export class PokemonDetailsComponent implements OnInit {
   private readonly pokemonService = inject(PokemonsService);
   private readonly route = inject(ActivatedRoute);
+  private readonly router = inject(Router);
 
   public pokemonId = this.route.snapshot.params?.['id'];
 
@@ -19,5 +37,9 @@ export class PokemonDetailsComponent implements OnInit {
 
   ngOnInit(): void {
     console.log('Pokemon ID :', this.pokemonId);
+  }
+
+  public goBack(): void {
+    this.router.navigateByUrl('/pokemon-list');
   }
 }
